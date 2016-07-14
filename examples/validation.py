06836f48e32e518d70fe6b9a66c4de9ce289a34e
Stefan Antoni <stefan@antoni.io>
2016-07-14 15:04:38 +0200

    Use re.VERBOSE to improve coding style

--- examples/validation.py
+++ examples/validation.py
@@ -1,10 +1,11 @@
 import re
 
 
-email_re = re.compile(
-    r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
-    r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"' # quoted-string
-    r')@(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?$', re.IGNORECASE)  # domain
+email_re = re.compile(r'''
+    (^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*  # dot-atom
+    |^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"  # quoted string
+    )@(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?$  # domain.tld
+''', re.IGNORECASE | re.VERBOSE)
 
 def save(obj):
     return "i saved your stuff"
@@ -25,5 +26,5 @@ def save_email(addr):
     print(dir(r))
 
 if __name__ == '__main__':
-    save_email(addr="decorate<at>yourhome.net")
-#    save_email(addr="decorate@yourhome.net")
+    # save_email(addr="decorate<at>yourhome.net")
+    save_email(addr="decorate@yourhome.org")
